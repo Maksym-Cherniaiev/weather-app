2689f62a7fe2166103646161f9737c09065fc54c
Maksym <likeran2@gmail.com>
2019-07-24 21:07:37 +0300

    Updates

---
+++ precache-manifest.d3e7b153038b73757bf9472efab535f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e51ff78e2e148adda015cf26ca91a55",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3bd4f461eefcc0ad2338",
+    "url": "/weather-app/static/css/main.167f4557.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/weather-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "3bd4f461eefcc0ad2338",
+    "url": "/weather-app/static/js/main.d12b4968.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
